6a63daa6972347fae3f4a35f43499e1690fc4066
Markus Zhang <markuszhang8@gmail.com>
2020-11-14 18:05:59 +0800

    ping route done

--- websocket/routes/ping.js
+++ websocket/routes/ping.js
@@ -1,7 +1,15 @@
-async (data, socket) => {
+/**
+ * User action to join the set class in the meeting by the teacher
+ * Lets Edupal record and persist data after meeting ends for dashboard
+ * @param {*} data
+ * @param {*} socket
+ */
+const ping = async (_, socket) => {
   try {
     await socket.send(JSON.stringify({ action: "PING" }), socket.id);
   } catch (err) {
-    console.log("Ping failed - old DB");
+    throw new Error(`Ping failed with id ${socket.id} at ${new Date()}`, err);
   }
 };
+
+module.exports = ping;
