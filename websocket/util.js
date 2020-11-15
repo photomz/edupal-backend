5c35a142dd476772eb83fac349bfa30ed0bc6950
Markus Zhang <markuszhang8@gmail.com>
2020-11-15 17:58:13 +0800

    Add queryUsers to util.js

--- websocket/util.js
+++ websocket/util.js
@@ -20,7 +20,62 @@ const typed = (schema, vari, options) => {
     );
 };
 
+/**
+ * Query all connected students or teachers in meeting
+ * Internal function to emit action to "everyone else"
+ * @param {*} role
+ * @param {*} meeting
+ * @returns {Array<string>}
+ */
+const queryUsers = async (role, meetingId) => {
+  const skStart =
+    // eslint-disable-next-line no-nested-ternary
+    role === "student"
+      ? "CONN#STUDENT#"
+      : role === "teacher"
+      ? "CONN#TEACHER#"
+      : "CONN#";
+
+  const response = await docClient.query({
+    TableName: process.env.db,
+    ExpressionAttributeValues: {
+      ":pk": `MEETING#${meetingId}`,
+      ":sk": skStart,
+    },
+    KeyConditionExpression: "pk = :pk and begins_with(sk, :sk)",
+    ProjectionExpression: "sk",
+  });
+
+  const users = response.Items.map(
+    ({ sk }) => sk.split("#")[sk.split("#").length - 1]
+  );
+
+  return users;
+};
+
+/**
+ * Emit a message for each connection
+ * @param {Array<string>} connections
+ * @param {*} payload
+ * @param {*} socket
+ */
+const emitForEach = async (connections, payload, socket) => {
+  const emitPromises = connections.map((el) =>
+    (async (id) => {
+      try {
+        await socket.send(payload, id);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log("Message could not be sent - CLIENT DISCONNECTED");
+      }
+    })(el)
+  );
+  await Promise.all(emitPromises);
+};
+
 module.exports = {
   docClient,
   typed,
+  queryUsers,
+  emitForEach,
 };
